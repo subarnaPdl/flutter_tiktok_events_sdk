2cba47061050243962c82ad4e92f87a80133c9f3
Marcelo Moresco <marcelo.moresco@koin.com.br>
2025-01-25 00:26:15 -0300

    fix: tiktok events sdk test

--- test/tiktok_events_sdk_method_channel_test.dart
+++ test/tiktok_events_sdk_method_channel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tiktok_events_sdk/src/bridge/tiktok_events_sdk_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelTiktokEventsSdk platform = MethodChannelTiktokEventsSdk();
   const MethodChannel channel = MethodChannel('tiktok_events_sdk');
 
   setUp(() {
