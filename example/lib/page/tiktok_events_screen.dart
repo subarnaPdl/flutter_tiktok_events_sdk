01b2d08fac3de2719cb6892e5957921764e23d56
Marcelo Moresco <marcelomoresco0@gmail.com>
2025-04-25 21:41:50 -0300

    refactor: remove unused import

--- example/lib/page/tiktok_events_screen.dart
+++ example/lib/page/tiktok_events_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:tiktok_events_sdk_example/services/tiktok_services.dart';
 
 class TikTokEventsPage extends StatefulWidget {
